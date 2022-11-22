bd89744f18a2a55118ab40011d92ea97d004bb95
Gaëtan <61387206+GaetanOff@users.noreply.github.com>
2022-11-22 15:46:24 +0100

    Create MessageEvent.ts

---
+++ src/modules/auto_answer/events/MessageEvent.ts
@@ -0,0 +1,19 @@
+import {BaseEvent, Event} from 'ioc:factory/Core/Event'
+import {GuildMember, Message, Role} from 'discord.js'
+
+const config = require('../../../../config.json');
+
+@Event('messageCreate')
+export default class MessageEvent extends BaseEvent {
+    public async run(message: Message): Promise<void> {
+        if (message.content.toLowerCase().includes("quoi")) {
+            message.react("🇫").then(async () => {
+                message.react("🇪").then(async () => {
+                    message.react("🇺").then(async () => {
+                        await message.react("🇷");
+                    })
+                })
+            })
+        }
+    }
+}
